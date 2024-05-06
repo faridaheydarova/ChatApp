9b4c636ffad7b7f06dc7e911febf3e16564cd5c2
farida2498 <125817327+faridaheydarova@users.noreply.github.com>
2024-05-06 22:17:04 +0400

    step 3
    disconnected eventlistener

--- chat/src/main/java/com/farida/chat/config/WebSocketEventListener.java
+++ chat/src/main/java/com/farida/chat/config/WebSocketEventListener.java
@@ -1,9 +1,15 @@
 package com.farida.chat.config;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import com.farida.chat.chat.ChatMessage;
+import com.farida.chat.chat.MessageType;
+
+import ch.qos.logback.core.subst.Token.Type;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,13 +17,22 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
-
+ final SimpMessageSendingOperations messageTemplate;
 	@EventListener
 	public void handleWebSocketDisconnectListener(
 			SessionDisconnectEvent event
 			)
 	{
-		// TODO -- to be implemented
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		if(username != null) {
+			log.info("User disconnected: {}", username);
+			var chatMessage=ChatMessage.builder()
+			.type(MessageType.LEAVE)
+			.sender(username)
+			.build();
+			messageTemplate.convertAndSend("/topic/public",chatMessage);
+		}
 	}
 
 }
